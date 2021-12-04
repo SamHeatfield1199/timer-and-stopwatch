92b868174cf387e83efafda5336011c84f00023c
Smirnova Anastasia <56282063+SamHeatfield1199@users.noreply.github.com>
2021-12-04 22:51:02 +1000

    Add files via upload

---
+++ stopwatch/js/Timer.js
@@ -0,0 +1,95 @@
+export default class Timer {
+  constructor(root) {
+    root.innerHTML = Timer.getHTML();
+
+    this.el = {
+      minutes: root.querySelector(".timer__part-minutes"),
+      seconds: root.querySelector(".timer__part-seconds"),
+      stopStart: root.querySelector(".timer__btn-controls"),
+      reset: root.querySelector(".timer__btn-reset"),
+    };
+
+    console.log(this.el);
+
+    this.interval = null;
+    this.remainingSec = 0;
+
+    this.el.stopStart.addEventListener("click", () => {
+      if (this.interval === null) {
+        this.start();
+        document.getElementsByClassName("outer-circle")[0].style =
+          " background: radial-gradient(43.34% 55.31% at 93.34% 44.69%, #000000 0%, #910a0a 100% )";
+      } else {
+        document.getElementsByClassName("outer-circle")[0].style =
+          " background: radial-gradient(43.34% 55.31% at 93.34% 44.69%, #000000 0%, #09A65A 100% )";
+        this.stop();
+      }
+    });
+    this.el.reset.addEventListener("click", () => {
+      this.reset();
+      this.updateInterfaceTime();
+    });
+  }
+
+  updateInterfaceTime() {
+    const minutes = Math.floor(this.remainingSec / 60);
+    const seconds = this.remainingSec % 60;
+
+    this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+    this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+  }
+
+  updateInterfaceControls() {
+    if (this.interval === null) {
+      this.el.stopStart.innerHTML = `<span class="timer__btn-control">start</span>`;
+    } else {
+      this.el.stopStart.innerHTML = `<span class="timer__btn-control">stop</span>`;
+    }
+  }
+
+  start() {
+    if (this.remainingSec === 3600) return;
+
+    this.interval = setInterval(() => {
+      this.remainingSec++;
+      this.updateInterfaceTime();
+
+      if (this.remainingSec === 3600) {
+        this.stop();
+      }
+    }, 1000);
+
+    this.updateInterfaceControls();
+  }
+
+  stop() {
+    clearInterval(this.interval);
+    this.interval = null;
+    this.updateInterfaceControls();
+  }
+
+  reset() {
+    this.remainingSec = 0;
+    clearInterval(this.interval);
+    this.interval = null;
+    this.updateInterfaceControls();
+  }
+
+  static getHTML() {
+    return `
+        <div class="timer__time">
+          <span class="timer__part-minutes"> 00 </span>
+          <span class="timer__part">:</span>
+          <span class="timer__part-seconds"> 00 </span>
+        </div>
+        <div class="timer__btns"> 
+          <button type="button" class="timer__btn timer__btn-controls">
+            <span class="timer__btn-control">start</span>
+          </button>
+          <button type="button" class="timer__btn  timer__btn-reset">
+          <span class="timer__btn-control">reset</span>
+          </button>
+        </div>
+        `;
+  }
+}
